8cc48443e7893f7b753eba53fda6286d5a25f733
Joshn't <joshua-hank@web.de>
2025-09-08 18:06:50 +0200

    Changed to FMOD 2.03.09

--- Assets/Plugins/FMOD/src/StudioListener.cs
+++ Assets/Plugins/FMOD/src/StudioListener.cs
@@ -78,11 +78,6 @@ namespace FMODUnity
             return result;
         }
 
-        public void ClearAttenuationObject()
-        {
-            AttenuationObject = null; // this disables the override
-        }
-
         private static void AddListener(StudioListener listener)
         {
             // Is the listener already in the list?
